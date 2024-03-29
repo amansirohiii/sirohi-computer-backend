b8b2cac2efe7dbbb2bdfde2139df9b2504d71ea5
Aman Sirohi <amansirohi077@gmail.com>
2024-03-29 19:30:26 +0530

    multer update

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
   });
-  const upload = multer({ dest: 'uploads/' }); // Temporary storage for uploaded files
+  const upload = multer({ storage: storage });
+
 
 
 
